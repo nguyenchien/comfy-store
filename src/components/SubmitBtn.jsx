09c3ea4f87bcbb9f73c63748f301d093939229c0
nguyen.van.chien <nguyen.van.chien@m-hand.com>
2023-11-09 10:56:48 +0700

    Submit Button

--- src/components/SubmitBtn.jsx
+++ src/components/SubmitBtn.jsx
@@ -1,6 +1,20 @@
+import { useNavigation } from "react-router"
+
 const SubmitBtn = ({text}) => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+  
   return (
-    <button type="submit" className="btn btn-primary btn-block">{text}</button>
+    <button type="submit" className="btn btn-primary btn-block" disabled={isSubmitting}>
+      {
+        isSubmitting ?
+          <>
+            <span className="loading loading-spinner"></span> sending...
+          </> 
+        :
+        text || 'submit'
+      }
+    </button>
   )
 }
 
