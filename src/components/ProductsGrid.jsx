193b48899a47531aa3598b8a3ff52d6428bad55e
nguyen.van.chien <nguyen.van.chien@m-hand.com>
2023-11-10 10:51:22 +0700

    format price

--- src/components/ProductsGrid.jsx
+++ src/components/ProductsGrid.jsx
@@ -1,4 +1,5 @@
 import { Link, useLoaderData } from 'react-router-dom';
+import { formatPrice } from '../utils';
 
 const ProductsGrid = () => {
   const { products } = useLoaderData();
@@ -24,7 +25,7 @@ const ProductsGrid = () => {
             </figure>
             <div className='card-body items-center text-center'>
               <h2 className='card-title capitalize tracking-wider'>{title}</h2>
-              <span className='text-secondary'>{dollarsAmount}</span>
+              <span className='text-secondary'>{formatPrice(dollarsAmount)}</span>
             </div>
           </Link>
         );
