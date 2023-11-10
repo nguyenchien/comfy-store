a0274b5c42737f55bd57aa634348d577011148a2
nguyen.van.chien <nguyen.van.chien@m-hand.com>
2023-11-10 11:57:36 +0700

    generateAmountOptions

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,5 +1,5 @@
 import { useLoaderData } from 'react-router-dom';
-import { formatPrice, customFetch } from '../utils';
+import { formatPrice, customFetch, generateAmountOptions } from '../utils';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
@@ -78,11 +78,7 @@ const SingleProduct = () => {
               value={amount}
               onChange={handleAmount}
             >
-              <option value={1}>1</option>
-              <option value={2}>2</option>
-              <option value={3}>3</option>
-              <option value={4}>4</option>
-              <option value={5}>5</option>
+              { generateAmountOptions(5) }
             </select>
           </div>
           {/* CART BUTTON */}
