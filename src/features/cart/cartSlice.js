aaebfe58612ddd691ba11c724dadd7ba82af9c47
chienvn <chiennguyen1702@gmail.com>
2023-11-26 22:07:31 +0700

    redux toolkit setup

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+
+const defaultState = {
+  cartItems: [],
+  numItemsInCart: 0,
+  cartTotal: 0,
+  shipping: 500,
+  tax: 0,
+  orderTotal: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: defaultState,
+  reducers: {
+    addItem: (state, action) => {
+      
+    },
+    clearCart: (state) => {
+      
+    },
+    removeItem: (state, action) => {
+      
+    },
+    editItem: (state, action) => {
+      
+    },
+  }
+});
+
+export const { addItem, clearCart, editItem, removeItem } = cartSlice.actions;
+export default cartSlice.reducer;
