d099ee3b2d7fa2e6aff3fba48911147ca4206579
chienvn <chiennguyen1702@gmail.com>
2024-01-20 12:40:31 +0700

    refactor theme toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,16 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
 import { BsCart3, BsMoonFill, BsSunFill } from 'react-icons/bs';
 import { FaBarsStaggered } from 'react-icons/fa6';
 import { NavLink } from 'react-router-dom';
 import NavLinks from './NavLinks';
-import { useSelector } from 'react-redux';
-
-const themes = {
-  winter: 'winter',
-  dracula: 'dracula',
-}
-
-const getThemeFromLocalStorage = () => {
-  return localStorage.getItem('theme') || themes.winter;
-}
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleTheme } from '../features/user/userSlice';
 
 const Navbar = () => {
-  const [theme, setTheme] = useState(getThemeFromLocalStorage());
+  const dispatch = useDispatch();
   
   const handleTheme = () => {
-    const { winter, dracula }  = themes;
-    const newTheme = theme === winter ? dracula : winter;
-    setTheme(newTheme);
-  }
-  
-  useEffect(()=>{
-    document.documentElement.setAttribute('data-theme', theme);
-    localStorage.setItem('theme', theme);
-  }, [theme]);
+    dispatch(toggleTheme());
+  };
   
   const numItemsInCart = useSelector((state) => state.cartState.numItemsInCart);
   
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,25 +1,41 @@
 import {  createSlice } from '@reduxjs/toolkit'
 import { toast } from 'react-toastify'
 
+const themes = {
+  winter: 'winter',
+  dracula: 'dracula',
+}
+
+const getThemeFromLocalStorage = () => {
+  const theme = localStorage.getItem('theme') || themes.winter;
+  document.documentElement.setAttribute('data-theme', theme);
+  return theme;
+}
+
 const initialState = {
   user: {
     username: 'coding addict',
-    theme: 'dracula',
+    theme: getThemeFromLocalStorage(),
   }
 }
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
-  loginUser: (state, action) => {
-    console.log('login user');
-  },
-  logoutUser: (state) => {
-    console.log('logout user');
-  },
-  toggleTheme: (state) => {
-    console.log('toggle theme');
-  },
+  reducers: {
+    loginUser: (state, action) => {
+      console.log('login user');
+    },
+    logoutUser: (state) => {
+      console.log('logout user');
+    },
+    toggleTheme: (state) => {
+      const { winter, dracula }  = themes;
+      state.theme = state.theme === winter ? dracula : winter;
+      document.documentElement.setAttribute('data-theme', state.theme);
+      localStorage.setItem('theme', state.theme);
+    },
+  }
 })
 
 export const { loginUser, logoutUser, toggleTheme } = userSlice.actions;
