fc9250eeab519927df70925862d20604ecb6bdfe
chienvn <chiennguyen1702@gmail.com>
2024-02-04 21:53:53 +0700

    setup checkout page

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,4 +1,23 @@
+import { useSelector } from "react-redux";
+import {CheckoutForm, SectionTitle, CartTotals} from "../components";
+
 const Checkout = () => {
-  return <h1 className='text-4xl'>Checkout</h1>;
+  const cartTotal = useSelector((state)=>{
+    return state.cartState.cartTotal;
+  });
+  
+  if (cartTotal === 0) {
+    return <SectionTitle text="Your cart is empty" />;
+  }
+  
+  return (
+    <>
+      <SectionTitle text="place your order" />
+      <div className="mt-8 grid gap-8 md:grid-cols-2 items-start">
+        <CheckoutForm />
+        <CartTotals />
+      </div>
+    </>
+  )
 };
 export default Checkout;
