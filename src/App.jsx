1c22452eef5a7fabf0df6bca469e5b5ed07808f2
chienvn <chiennguyen1702@gmail.com>
2024-02-06 22:06:59 +0700

    checkout form

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ import { loader as checkoutLoader } from './pages/Checkout';
 // action
 import {action as registerAction} from './pages/Register';
 import {action as loginAction} from './pages/Login';
+import {action as checkoutAction} from './components/CheckoutForm';
 import {store} from './store';
  
  const router = createBrowserRouter([
@@ -61,6 +62,7 @@ import {store} from './store';
         path: '/checkout',
         element: <Checkout />,
         loader: checkoutLoader(store),
+        action: checkoutAction(store),
       },
       {
         path: '/orders',
