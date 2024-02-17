426bdd542816c25e6a0fe196e2bdb227fc1d338e
chienvn <chiennguyen1702@gmail.com>
2024-02-17 16:43:21 +0700

    order list

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -35,7 +35,7 @@ export const action = (store) => async({request}) => {
   } catch (error) {
     const errorMessage = error?.response?.data?.error?.message || 'there was an error placing your order';
     toast.error(errorMessage);
-    if (error.response.status === 401) return redirect('/login');
+    if (error.response.status === 401 || 403) return redirect('/login');
     return null;
   }
 }
